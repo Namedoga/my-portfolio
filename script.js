bd151937da3ddc88f26bdf8cfe8c103c564d5411
Othniel Powell <othniel@live.ca>
2024-12-04 17:33:32 -0500

    added bouce thru animate

--- script.js
+++ script.js
@@ -65,3 +65,21 @@ function validateForm() {
     darkModeToggleButton.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
     });
+  
+// Filter button 
+
+const filterButtons = document.querySelectorAll('.filter-btn');
+const projects = document.querySelectorAll('.project');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const category = this.getAttribute('data-category');
+        projects.forEach(project => {
+            if (project.getAttribute('data-category') === category || category === 'all') {
+                project.style.display = 'block';
+            } else {
+                project.style.display = 'none';
+            }
+        });
+    });
+});
